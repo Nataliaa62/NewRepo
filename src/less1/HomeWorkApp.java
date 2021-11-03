1274417a4affe4971933485103cffe0fc5a53043
nataliaa <nataliaa62@yandex.ru>
2021-11-03 12:10:14 +0300

    Доавила ДЗ3

--- src/less1/HomeWorkApp.java
+++ src/less1/HomeWorkApp.java
@@ -4,31 +4,59 @@ public class HomeWorkApp {
 
     public static void main(String[] args) {
         printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
 
+        System.out.println("****************************************");
+
+
+        if (checkSumSign(10,5)) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        };
+
+        System.out.println("****************************************");
+
+
+        System.out.println (printColor(56));
+        System.out.println (printColor(-4));
+
+        System.out.println("****************************************");
+
+        if (compareNumbers(5,6)) {
+            System.out.println("a>=b");
+        } else {
+            System.out.println("a<b");
+        }
     }
 
+    // Задание 1. Мой вариант
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
 
+// Задание 2. Мой вариант
+//    public  static void checkSumSign () {
+//        int a = 450;
+//        int b = -350;
+//        int c = a + b;
+//        if (c >= 0){
+//            System.out.println("Сумма положительная");
+//        } else {
+//            System.out.println("Сумма отрицательная");
+//        }
+//
+//    }
 
-    public  static void checkSumSign () {
-        int a = 450;
-        int b = -350;
-        int c = a + b;
-        if (c >= 0){
-            System.out.println("Сумма положительная");
-        } else {
-            System.out.println("Сумма отрицательная");
-        }
-
+    // Задание 2. Вариант Александра
+    public  static boolean checkSumSign (int a, int b) {
+        return a + b>=0;
     }
 
+
+
+    // Задание 3. Мой вариант
     public static void printColor () {
         int value = 340;
         if (value <= 0) {
@@ -41,15 +69,36 @@ public class HomeWorkApp {
 
     }
 
-    public static void compareNumbers() {
-        int a = 16;
-        int b = 15;
-        if (a >= b){
-            System.out.println("a>=b");
+    // Задание 3. Вариант Александра
+    public static String printColor (int value) {
+        if (value <= 0) {
+            return "Красный";
+        } else if (value > 0 && value <= 100) {
+            return "Желтый";
         } else {
-            System.out.println("a<b");
+            return "Зеленый";
         }
+
     }
+
+// Задание 4. Мой вариант
+//  public static void compareNumbers() {
+//        int a = 16;
+//      int b = 15;
+//        if (a >= b){
+//        System.out.println("a>=b");
+//        } else {
+//            System.out.println("a<b");
+//        }
+//
+
+
+    // Задание 4. Вариант Александра
+    public static boolean compareNumbers(int a, int b) {
+        return a >= b;
+
+    }
+
 
 
 }
